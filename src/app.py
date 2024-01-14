710548d3a145fe652cf549e53a3055a8f19ba588
cesullivan99 <cesullivan99@gmail.com>
2024-01-14 14:43:25 -0500

    reverting table change

--- src/app.py
+++ src/app.py
@@ -23,15 +23,10 @@ Use curse words frequently.
 def login():
     data = request.json
     userId = data.get('userId')
-    
-    # Check if userId already exists in user_chat_map
-    if userId not in user_chat_map:
-        add_to_map(userId)
-        print("user id " + userId + " added to map!")
-        message = "Login successful. User was added to the map."
-    else:
-        print("user id " + userId + " already in map!")
-        message = "Login successful. User was already in the map."
+ 
+    add_to_map(userId)
+    print("user id " + userId + " added to map!")
+    message = "Login successful. User was added to the map."
 
     return jsonify({"message": message}), 200
 
