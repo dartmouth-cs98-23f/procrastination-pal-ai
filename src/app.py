d61132c93576f390a3c7aacc6498c59f1b47e507
Nick Luikey <nicholas.j.luikey.24@dartmouth.edu>
2024-02-26 00:15:43 -0500

    change 6 to 5 (not sure if it does anything)

--- src/app.py
+++ src/app.py
@@ -104,12 +104,12 @@ def get_personality(responses):
     if data == None:
         personality_response = "Tough love"
     # If shorter user survey
-    if len(data['responselist']) < 6:
+    if len(data['responselist']) < 5:
         personality_response = next((item['response'] for item in data['responselist'] if item['questionId'] == 4), "Tough love")
     else:
-        # Find the full response text for questionId 6, substituting
-        personality_response = next((item['response'] for item in data['responselist'] if item['questionId'] == 6), "Tough love")
-    print("response for question 6 full text: " + personality_response)
+        # Find the full response text for questionId 5, substituting
+        personality_response = next((item['response'] for item in data['responselist'] if item['questionId'] == 5), "Tough love")
+    print("response for question 5 full text: " + personality_response)
 
     # Adjusted to match the enum based on the actual response text
     personality = ""
